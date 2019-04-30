ecbd96ed2234fd61727f00db76bb3effe1467bf9
Elias Smeds <eliassmeds@Eliass-MacBook-Pro.local>
2019-04-30 16:14:39 +0200

    Initial commit

---
+++ src/main/java/com/techprimers/db/resource/UsersResource.java
@@ -0,0 +1,29 @@
+package com.techprimers.db.resource;
+
+import com.techprimers.db.model.Users;
+import com.techprimers.db.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/rest/users")
+public class UsersResource {
+
+    @Autowired
+    UsersRepository usersRepository;
+
+    @GetMapping(value = "/all")
+    public List<Users> getAll() {
+        return usersRepository.findAll();
+    }
+
+    @PostMapping(value = "/load", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public List<Users> persist(@RequestBody final Users users) {
+        usersRepository.save(users);
+        return usersRepository.findAll();
+    }
+
+}
